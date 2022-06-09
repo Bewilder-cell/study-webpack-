f9d8cedf4b59daad2ffc1446df8add6b9b389096
bewilder-cell <a1@1deMacBook-Pro.local>
2022-06-09 21:15:48 +0800

    week 1

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,12 @@ output: {
 mode: 'development',
 module:{
     rules:[
-        {test:/.js$/,   //匹配所有.js文件
-        use:'babel-loader'},{
+    {  
+        test:/.js$/,   //匹配所有.js文件
+        use: 'babel-loader?cacheDirectory', // 缓存loader执行结果 发现打包速度已经明显提升了
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src') // 精确指定要处理的目录
+    },{
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
     }
